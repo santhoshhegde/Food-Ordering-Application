f24e754c251e15a88e162ce50e22adac86b8bac1
Santhosh Hegde <santhosh.hegde262000@gmail.com>
2024-05-27 07:56:25 +0530

    styled resturant card and img

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -2,10 +2,16 @@ import { imga } from "../../utils/constants";
 
 const Restaurant = ({ name, cuisines, avgRating, cloudinaryImageId }) => {
   return (
-    <div className="w-52 p-3 m-3 bg-pink-50 shadow-lg">
-      <img src={imga + cloudinaryImageId} alt="img" />
+    <div className="w-72 h-52 m-3 bg-pink-50 shadow-lg rounded-3xl">
+      <img
+        className="h-full w-full object-fill rounded-3xl"
+        src={imga + cloudinaryImageId}
+        alt="img"
+      />
       <h3>{name}</h3>
-      <h4 className="overflow-hidden w-full">{cuisines.join(", ")}</h4>
+      <h4 className="whitespace-nowrap w-full overflow-hidden overflow-ellipsis">
+        {cuisines.join(", ")}
+      </h4>
       <h5>{avgRating}</h5>
     </div>
   );
